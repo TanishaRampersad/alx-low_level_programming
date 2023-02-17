4f6bf2bf4c29d888d8290a231aee95723a2d0902
Tanisha Rampersad <Tanisharampersad24@gmail.com>
2023-02-17 04:12:31 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main -Entry point
+* Return: Always 0 (success0
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			C--;
+		}
+	putchar('\n');
+	return (0);
+}
