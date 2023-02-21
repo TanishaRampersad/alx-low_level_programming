9b1483a99c0721d2d2a75625e4d7a36613364dfb
Tanisha Rampersad <Tanisharampersad24@gmail.com>
2023-02-21 03:59:03 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,8 +6,7 @@
 * Return: 1 for positive num, -1 for negative num, 0 for anything else
 */
 
-int print_sign(int n)
-{
+int print_sign(int n){
 	if (n > 0)
 	{
 		_putchar('+');
@@ -18,7 +17,7 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	else if (n == 0)
+	else
 	{
 		_putchar('0');
 		return (0);
