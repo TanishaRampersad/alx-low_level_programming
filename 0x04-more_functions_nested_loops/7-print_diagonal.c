2f469b3d1c28a6d556545529fd7b181712ef6e8c
Tanisha Rampersad <Tanisharampersad24@gmail.com>
2023-02-23 04:25:17 -0800

    updated 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,24 +8,22 @@
 
 void print_diagonal(int n)
 {
-        int x, y;
+	int x, y;
 
-
-        if (n <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                for (x = 1; x < n; x++)
-                {
-                for (y = 1; y < x; y++)
-                {       
-                        _putchar(' ');
-                }       
-                _putchar('\\');
-                _putchar('\n');
-                }
-        }       
-}      
-
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x < n; x++)
+		{
+		for (y = 1; y < x; y++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+}
