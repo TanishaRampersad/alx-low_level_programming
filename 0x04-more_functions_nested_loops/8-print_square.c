9660586a20a8b4924ae6054893293f679fffda28
Tanisha Rampersad <Tanisharampersad24@gmail.com>
2023-02-23 05:13:32 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,8 @@
 
 void print_square(int size)
 {
-	int x, y;
+	int x;
+	int y;
 
 	if (size <= 0)
 	{
@@ -16,11 +17,12 @@ void print_square(int size)
 	}
 	else
 	{
-		for (x = 0; x <= size; x++)
+		for (x = 1; x <= size; x++)
 		{
-			for (y = 0; y <= size; y++)
-			{
-				_putchar(35);
+			_putchar('#');
+		for (y = 2; y <= size; y++)
+		{
+			_putchar('#');
 			}
 			_putchar('\n');
 		}
