7ee5f99da8cd724bd0fe46cee877a46c0fc8aeef
Tanisha Rampersad <Tanisharampersad24@gmail.com>
2023-02-23 01:30:53 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiply 2 integers
+* @a: First integer
+* @b: Second integer
+* Return: addition of the multiplication
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
